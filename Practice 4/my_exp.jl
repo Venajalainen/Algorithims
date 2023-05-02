fd9f0891fc8c67aa6c474e3f10bdb6c09658e35c
Naumov Alexandr <naumovzaz@gmail.com>
2023-05-02 22:58:18 +0300

    adds

--- Practice 4/my_exp.jl
+++ Practice 4/my_exp.jl
@@ -2,13 +2,14 @@ include("..//Practice 2//fastpow.jl")
 
 function euler_sums(x :: Float64, n :: Int, eps :: Float64 = 1e-7)
     S :: Float64 = 1.
-    a :: Float64 = x
+    a :: Float64 = abs(x)
     k :: Int = 2
     while k-2<n && S+a!=S && abs(a)>eps
         S+=a
-        a = a*x/k
+        a = a*abs(x)/k
         k+=1
     end
+    x<0 && return 1/S
     return S
 end
 
