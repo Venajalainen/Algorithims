fbecdf7c287507a6906f998f386c7bf507233b27
Naumov Alexandr <naumovzaz@gmail.com>
2023-05-16 21:46:46 +0300

    practice 6

--- Practice 4/rang.jl
+++ Practice 4/rang.jl
@@ -4,7 +4,7 @@ function rang(_M :: AbstractMatrix{T}) where T
     M = to_step(_M);
     i :: Int, j :: Int = 1, 1
     rangM :: Int = 0
-    while i<=size(M,2) && j<=size(M,1)
+    while i<=size(M,1) && j<=size(M,2)
         if !iszero(M[i,j])
             rangM+=1;
             j+=1;
---
+++ Practice 6/structs.jl
@@ -0,0 +1,80 @@
+import Base: +,-,*,cos,sin,angle,sign
+import LinearAlgebra: norm, dot
+include("../Practice 4/determinant.jl")
+include("../Practice 4/rang.jl")
+
+struct Vector2D{T<:Real}
+    x :: T
+    y :: T
+end
+
++(a :: Vector2D{T}, b :: Vector2D{T}) where T = Vector2D{T}(a.x+b.x,a.y+b.y)
+-(a :: Vector2D{T}, b :: Vector2D{T}) where T = Vector2D{T}(a.x-b.x,a.y-b.y)
+*(a :: Vector2D{T}, b :: Vector2D{T}) where T = Vector2D{T}(a.x*b.x,a.y*b.y)
+cos(a :: Vector2D{T}, b :: Vector2D{T}) where T = dot(a,b)/(norm(a)*norm(b))
+sin(a :: Vector2D{T}, b :: Vector2D{T}) where T = xdot(a,b)/(norm(a)*norm(b))
+norm(a :: Vector2D{T}) where T = norm((a.x,a.y))
+dot(a :: Vector2D{T}, b :: Vector2D{T}) where T = a.x*b.x+a.y*b.y
+xdot(a :: Vector2D{T}, b :: Vector2D{T}) where T = a.x*b.y-a.y*b.x
+angle(a :: Vector2D{T}, b :: Vector2D{T}) where T = atan(sin(a,b)/cos(a,b))
+sign(a :: Vector2D{T}, b :: Vector2D{T}) where T = sign(sin(a,b))
+
+
+struct Segment2D{T <: Real}
+    p1 :: Vector2D{T}
+    p2 :: Vector2D{T}
+    Segment2D{T}(p1 :: NTuple{2,T}, p2 :: NTuple{2,T}) where T = new{T}(Vector2D(p1...),Vector2D(p2...))
+    Segment2D{T}(p1 :: Vector2D{T}, p2 :: Vector2D{T}) where T = new{T}(p1,p2)
+end
+
+to_origin(line :: Segment2D{T}) where T<:Real = line.p2-line.p1
+
+function sameside(line :: Segment2D{Vector2D{T}}, p1 :: Vector2D{T}, p2 :: Vector2D{T}) :: Bool where T
+    origin = to_origin(line)
+    return sign(origin,p1) == sign(origin,p2) || sign(origin,p1)*sign(origin,p2)==0
+end
+
+function sameside(f :: Function, p1 :: Vector2D{T}, p2 :: Vector2D{T}) :: Bool where T
+    return sign(f(p1.x,p1.y)) == sign(f(p2.x,p2.y)) || sign(f(p1.x,p1.y))*sign(f(p2.x,p2.y))==0
+end
+
+function intersection(line1 :: Segment2D{T}, line2 :: Segment2D{T}) where T<: Vector2D
+    𝚫x1 :: Float64,𝚫y1 :: Float64 = line1.p1.x-line1.p2.x, line1.p1.y-line1.p2.y
+    𝚫x2 :: Float64,𝚫y2 :: Float64 = line2.p1.x-line2.p2.x, line2.p1.y-line2.p2.y
+    M = [𝚫y1 -𝚫x1
+         𝚫y2 -𝚫x2]
+    B = [line1.p1.x*𝚫y1-line1.p1.y*𝚫x1
+         line1.p2.x*𝚫y1-line1.p2.y*𝚫x1
+        ]
+    if det(M) == 0
+        if rang(M) == rang([M B])
+            @warn("линии сопадают")
+        end
+        return nothing
+    end
+    return M\B
+end
+
+struct Polygon{T<:Real}
+    sides :: Vector{Segment2D{T}}
+    function Polygon{T}(points :: AbstractVector{Vector2D{T}}) where T<:Real
+        sides = Vector{Segment2D{T}}([])
+        for i in 1:length(points)-1
+            push!(sides,Segment2D{T}(points[i],points[i+1]))
+        end
+        push!(sides,Segment2D{T}(points[end],points[begin]))
+        return new{T}(sides)
+    end
+end
+
+function insidepolygon(p :: Vector2D{T}, poly :: Polygon{T}) where T<:Real
+    sum_angle :: T = zero(T)
+    @assert length(poly.sides)>2
+    for side in poly.sides
+        sum_angle+=angle(side.p2-p,side.p1-p)
+    end
+    println(sum_angle)
+    return sum_angle>pi
+end
+
+#points = [Vector2D{Real}(1,3),Vector2D{Real}(3,2),Vector2D{Real}(-1,-1)]
