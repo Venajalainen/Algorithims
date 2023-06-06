2724421a12467498a462cb8f588d4c3dc6fb99bb
Naumov Alexandr <naumovzaz@gmail.com>
2023-06-06 21:31:06 +0300

    smt in graphs

--- Practice 7/graphs.jl
+++ Practice 7/graphs.jl
@@ -52,46 +52,55 @@ function components(graph :: Dict{T, Vector{T}}) where T<:Integer
 end
 # является ли граф двудольным?
 function dual(graph :: Dict{T, Vector{T}}) where T
-    start :: Int = 1
-    mark = 1
-    paint = zeros(Int,length(graph))
-    queue = Queue{T}()
-    enqueue!(queue, start)
-    paint[start] = mark
-    while !isempty(queue)
-        v = dequeue!(queue)
-        mark = max(mod(mark+1,3),1)
-        for v′ in graph[v]
-            if iszero(paint[v′])
-                enqueue!(queue, v′)
-                paint[v′] = mark 
-            else
-                paint[v'] != mark && return false
+    i :: Union{Int, Nothing} = 1
+    paint :: Vector{Int} = zeros(length(graph))
+
+    while !isnothing(i)
+        stack = [i]
+        mark :: Int = 1
+        paint[i] = mark
+
+        while !isempty(stack)
+            v = pop!(stack)
+            mark = max(1,mod(paint[v]+1,3))
+            for v′ in graph[v]
+                if iszero(paint[v′])
+                    push!(stack,v′)
+                    paint[v′] = mark
+                else
+                    paint[v′] == paint[v] && begin println(paint); return false end
+                end
             end
+        end
+
+        i = findfirst(paint) do x
+            return iszero(x)
         end
     end
     return true
 end
 
-# testree = Dict{Int,Vector{Int}}()
-# testree[1] = [2,3]
-# testree[2] = [4]
-# testree[4] = [2]
-# testree[3] = []
+"""
+testree = Dict{Int,Vector{Int}}()
+testree[1] = [2,3]
+testree[2] = [4]
+testree[4] = [2]
+testree[3] = []
 
-# testree = Dict{Int,Vector{Int}}()
-# testree[1] = [2,3]
-# testree[2] = [1,3]
-# testree[3] = [1,2]
-# testree[4] = [5,7]
-# testree[5] = [4,6]
-# testree[6] = [5,7]
-# testree[7] = [4,6]
+testree = Dict{Int,Vector{Int}}()
+testree[1] = [2,3]
+testree[2] = [1,3]
+testree[3] = [1,2]
+testree[4] = [5,7]
+testree[5] = [4,6]
+testree[6] = [5,7]
+testree[7] = [4,6]
 
-# testree = Dict{Int,Vector{Int}}()
-# testree[1] = [2]
-# testree[2] = [3]
-# testree[3] = [4]
-# testree[4] = [4,5]
-# testree[5] = [6]
-# testree[6] = []
+testree = Dict{Int,Vector{Int}}()
+testree[1] = [2]
+testree[2] = [3]
+testree[3] = [4]
+testree[4] = [4,5]
+testree[5] = [6]
+testree[6] = []
+"""
